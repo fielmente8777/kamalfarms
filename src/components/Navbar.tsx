384921eea0138fac32c830c9c742e1aab43656a2
sumit <sk7627577@gmail.com>
2024-12-21 13:52:32 +0530

    refactor: update Navbar component with dynamic links and remove unused imports

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,42 @@
 import Link from "next/link";
 import Container from "./Container";
-import { IoIosCall } from "react-icons/io";
-import { SiMinutemailer } from "react-icons/si";
+import { NavLink } from "@/data/navbar";
+import { DropDown } from "@/icons/icons";
 const Navbar = () => {
   return (
     <header>
       <Container>
-        <nav className="flex justify-between items-center py-6">
+        <nav className="flex justify-between items-center">
           <div>logo</div>
           <div className="flex items-center gap-4 text-base">
-            <Link href="href" className="flex items-center gap-2">
-              <span className="">
-                <IoIosCall size={20} />
-              </span>
-              <span className="lg:block hidden">+234 123 456 7890</span>
-            </Link>
-            <Link href="href" className="flex items-center gap-2">
-              <span>
-                <SiMinutemailer size={20} />
-              </span>
-              <span className="lg:block hidden">WmF8B@example.com</span>
-            </Link>
+            <ul className="flex items-center gap-4 text-base relative">
+              {NavLink.slice(1, NavLink.length - 1).map((link) => (
+                <li key={link.id} className="group relative text-primary">
+                  <Link href={link.link} className="flex items-center justify-center gap-2 px-4 py-2">
+                    {link.name}{" "}
+                    {link.subLinks && (
+                      <span className="ml-2">
+                        <DropDown />
+                      </span>
+                    )}
+                  </Link>
+                  {link.subLinks && (
+                    <ul className="absolute left-0 w-max hidden group-hover:block bg-white shadow-lg">
+                      {link.subLinks.map((subLink) => (
+                        <li key={subLink.id}>
+                          <Link href={subLink.link} className="block px-4 py-2">
+                            {subLink.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
+            <button className="bg-primary text-white px-6 py-5">
+              Contact Us
+            </button>
           </div>
         </nav>
       </Container>
