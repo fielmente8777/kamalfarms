780d460c518439a2ab0a2742f5f78f06e1602161
sumit <sk7627577@gmail.com>
2024-12-21 14:17:11 +0530

    feat: enhance Navbar with mobile support and dynamic logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,37 @@
+"use client";
 import Link from "next/link";
 import Container from "./Container";
 import { NavLink } from "@/data/navbar";
 import { DropDown } from "@/icons/icons";
+import Image from "next/image";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
       <Container>
         <nav className="flex justify-between items-center">
-          <div>logo</div>
-          <div className="flex items-center gap-4 text-base">
+          <div className="flex items-center gap-4">
+            <Link href="/" className="relative h-20 aspect-[4/2]">
+              <Image
+                src="/Logo.svg"
+                alt="Kamalfarms"
+                fill
+                className="object-contain"
+              />
+            </Link>
+          </div>
+          <div className="lg:flex hidden items-center gap-4 text-base">
             <ul className="flex items-center gap-4 text-base relative">
               {NavLink.slice(1, NavLink.length - 1).map((link) => (
                 <li key={link.id} className="group relative text-primary">
-                  <Link href={link.link} className="flex items-center justify-center gap-2 px-4 py-2">
+                  <Link
+                    href={link.link}
+                    className="flex items-center justify-center gap-2 px-4 py-2"
+                  >
                     {link.name}{" "}
                     {link.subLinks && (
                       <span className="ml-2">
@@ -38,6 +57,14 @@ const Navbar = () => {
               Contact Us
             </button>
           </div>
+          <div className="lg:hidden block">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className={`text-primary text-3xl`}
+            >
+              {isOpen ? <IoMdClose /> : <CiMenuBurger />}
+            </button>
+          </div>
         </nav>
       </Container>
     </header>
@@ -45,3 +72,43 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+export const MobileNavbar = () => {
+  return (
+    <header>
+      <Container>
+        <nav className="flex flex-col justify-between items-center">
+          {NavLink.slice(1, NavLink.length - 1).map((link) => (
+            <li key={link.id} className="group relative text-primary">
+              <Link
+                href={link.link}
+                className="flex items-center justify-center gap-2 px-4 py-2"
+              >
+                {link.name}{" "}
+                {link.subLinks && (
+                  <span className="ml-2">
+                    <DropDown />
+                  </span>
+                )}
+              </Link>
+              {link.subLinks && (
+                <ul className="absolute left-0 w-max hidden group-hover:block bg-white shadow-lg">
+                  {link.subLinks.map((subLink) => (
+                    <li key={subLink.id}>
+                      <Link href={subLink.link} className="block px-4 py-2">
+                        {subLink.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+          <button className="bg-primary text-white px-6 py-5">
+            Contact Us
+          </button>
+        </nav>
+      </Container>
+    </header>
+  );
+};
