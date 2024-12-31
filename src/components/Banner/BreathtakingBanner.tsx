8289cdb65c48c39d5032117c6fe2a62e6fc6c60e
sumit <sk7627577@gmail.com>
2024-12-31 19:04:30 +0530

    fix: adjust positioning for BreathtakingBanner component on smaller screens

--- src/components/Banner/BreathtakingBanner.tsx
+++ src/components/Banner/BreathtakingBanner.tsx
@@ -46,7 +46,7 @@ const BreathtakingBanner: React.FC<Props> = ({
           className={`col-span-1 max-md:relative ${index % 2 === 0 ? "order-2" : "order-1"}`}
         >
           <div
-            className={`lg:absolute ${contentClassName ? contentClassName : "bg-bgclr"} shadow-2xl lg:w-[46rem] w-[19rem] max-md:mx-auto ${index % 2 === 0 ? "lg:right-0" : "lg:left-0"} lg:bottom-[12%] relative -top-5 z-10 lg:p-12 py-7 px-4 flex ${!center ? "items-start justify-start" : "items-center justify-center"} gap-7 flex-col`}
+            className={`lg:absolute ${contentClassName ? contentClassName : "bg-bgclr"} shadow-2xl lg:w-[46rem] w-[19rem] max-md:mx-auto ${index % 2 === 0 ? "lg:right-0" : "lg:left-0"} lg:bottom-[12%] relative max-md:-top-5 z-10 lg:p-12 py-7 px-4 flex ${!center ? "items-start justify-start" : "items-center justify-center"} gap-7 flex-col`}
           >
             {icon && (
               <div>
